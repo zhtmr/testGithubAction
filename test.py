1a80a225f326481f51bd111cf37ab92088482dc1
csw <48509269+zhtmr@users.noreply.github.com>
2024-01-06 21:35:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/new-releases/2200'
+url = 'https://www.aladin.co.kr/shop/common/wbest.aspx?BestType=Bestseller&BranchType=1&CID=7396'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.title_text')
+bookservices = soup.select('.bo3')
 for no, book in enumerate(bookservices, 1):
     print(no, book.text.strip())
